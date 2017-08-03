1b3ec4eea05faaff5498443f1d0b3fc5e0814817
avaschuk <andreivashchuk4@gmail.com>
2017-08-03 17:22:32 +0300

    return

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+  console.log('5555555555555555555');
+});
 
 module.exports = router;
