0c569696b5cd5247e0c583435f38dd599c189ee4
Pavel <7doctor7@tut.by>
2017-08-03 17:16:51 +0300

    565656

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+  console.log('5555555555555555555');
 });
 
 module.exports = router;
