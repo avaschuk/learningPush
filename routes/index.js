571fe25755da592fc2e9b974fd1143de664114a2
Pavel <7doctor7@tut.by>
2017-08-03 18:04:31 +0300

    commit 66

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,6 @@ router.get('/hello', function (req, res) {
     res.status(200).send("Hello world!!");
     console.log('hello world');
 });
+  console.log('55555555555555555555555555');
 
 module.exports = router;
