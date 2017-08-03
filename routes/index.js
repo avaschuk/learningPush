a535f8805c8d63cb15021ba64c95652cfc0a1e9a
avaschuk <andreivashchuk4@gmail.com>
2017-08-03 17:52:24 +0300

    add adsd

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  console.log('adsd');
 });
 router.get('/hello', function (req, res) {
     res.status(200).send("Hello world!!");
